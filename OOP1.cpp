cc2cbbd7272b3630b80646456a538f8541709d6f
Aswun <aswinlutfian32@gmail.com>
2024-04-23 14:50:31 +0700

    menambahkan fungsi main

--- OOP1.cpp
+++ OOP1.cpp
@@ -32,9 +32,15 @@ public :
         cout << "SKS: ";
         cin >> sks;
     }
-        void output(){
+        void output()
+    {
         cout << "Kode MK: " << kodeMk << endl;
         cout << "Nama MK: " << namaMk << endl;
         cout << "SKS: " << sks << endl;
     }
+};
+
+int main() 
+{
+
 }
